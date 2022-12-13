7e64399118ec978939642f2a34561f5d1366fae7
root <alfie.alphonce@gmail.com>
2022-12-13 11:17:22 -0800

    task 5 to 9 done

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet in lowercase
+ * followed by anew line
+ * Return: Always 0
+ */
+int main(void)
+{
+	char la;
+
+	for (la = 'a'; la <= 'z'; la++)
+	{
+		if (la != 'e' && la != 'q')
+			putchar(la);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - print the alphabet in reverse
+ * Return: Always 0
+ */
+int main(void)
+{
+	char re;
+
+	for (re = 'z'; re >= 'a'; re--)
+		putchar(re);
+
+	putchar('\n);
+
+	return (0);
+}
