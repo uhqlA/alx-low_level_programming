749edff73c49934257a3f38773a9b9ae04f09d7b
root <alfie.alphonce@gmail.com>
2022-12-13 12:00:46 -0800

    task 10 edited

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,7 +10,7 @@ int main(void)
 
 	for (tens = 0; tens <= 9; tens++)
 	{
-		for (ones = tens + 1; ones <= 9; 0nes++)
+		for (ones = tens + 1; ones <= 9; ones++)
 		{
 			putchar(tens + '0');
 			putchar(ones + '0');
