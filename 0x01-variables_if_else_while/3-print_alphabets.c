ec951232ab00cc645a65ebf0c83292895a4044e2
root <alfie.alphonce@gmail.com>
2022-12-13 10:17:23 -0800

    two edited

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,7 +9,7 @@ int main(void)
 	char ch;
 	for (ch = 'a'; ch <= 'z'; ch++)
 		putchar(ch);
-	for (ch = 'A'; ch <= 'Z'; ch++ 0)
+	for (ch = 'A'; ch <= 'Z'; ch++)
 		putchar(ch);
 	putchar('\n');
 	return (0);
