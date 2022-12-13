2e2346eb2c27d2fae1fc2c9fb99d0e241ffe1822
root <alfie.alphonce@gmail.com>
2022-12-13 10:13:18 -0800

    two and three done

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - print alpha in lower and upper case
+ * Return: Always 0
+ */
+int main(void)
+{
+	char ch;
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
+	for (ch = 'A'; ch <= 'Z'; ch++ 0)
+		putchar(ch);
+	putchar('\n');
+	return (0);
+}
